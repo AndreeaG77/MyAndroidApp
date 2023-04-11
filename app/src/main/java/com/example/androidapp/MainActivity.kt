42e1a57f255ea23988d0439ae09d7b821534c3ae
AndreeaG <andreea.g2001@yahoo.com>
2023-04-11 22:16:48 +0300

    login page design

--- app/src/main/java/com/example/androidapp/MainActivity.kt
+++ app/src/main/java/com/example/androidapp/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.androidapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import com.google.android.material.button.MaterialButton
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val username: TextView = findViewById(R.id.username)
+        val password: TextView = findViewById(R.id.password)
+        val loginB: MaterialButton = findViewById(R.id.loginB)
     }
 }
